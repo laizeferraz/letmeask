d54eeb5015fcf18f95565362eb024109d0b7cd8c
Laize Ferraz <laizeferraz@gmail.com>
2021-07-22 16:05:24 +1000

    enter in a room with a code done.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import { useHistory } from "react-router-dom";
+import { FormEvent } from "react";
+import { useState } from "react";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
@@ -6,18 +8,36 @@ import googleImg from "../assets/images/google-icon.svg";
 
 import { Button } from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 
 import "../styles/auth.scss";
 
 export const Home = () => {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState("");
 
   async function handleCreateRoom() {
     if (!user) {
       await signInWithGoogle();
     }
     history.push("/rooms/new");
+  }
+
+  async function handleJoinRoom(e: FormEvent) {
+    e.preventDefault();
+
+    if (roomCode.trim() === "") {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("Room does not exists.");
+      return;
+    }
+    history.push(`/rooms/${roomCode}`);
   }
   return (
     <div id="page-auth">
@@ -34,8 +54,13 @@ export const Home = () => {
             Create your room with Google
           </button>
           <div className="separator">or enter a room</div>
-          <form>
-            <input type="text" placeholder="Type the room code" />
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Type the room code"
+              onChange={(e) => setRoomCode(e.target.value)}
+              value={roomCode}
+            />
 
             <Button>Get inside</Button>
           </form>
